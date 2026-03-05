2c03bd0a09756f68881a80c8ac2c5b520cbfe8f1
Onion <mchess563@gmail.com>
2026-03-05 09:33:12 +0700

    feat: add Transaction class with BORROW and RETURN constants and full logging support

--- Transaction.java
+++ Transaction.java
@@ -1,13 +1,31 @@
-class Transaction {
-    Member memberRef;    // object reference
-    Book bookRef;        // object reference
-    String transactionType;
-    int transactionId;
+public class Transaction {
+    // Transaction types as constants
+    public static final String BORROW = "BORROW";
+    public static final String RETURN = "RETURN";
 
-    Transaction(int transactionId, Member m, Book b, String type) {
+    private int transactionId;
+    private Member memberRef;
+    private Book bookRef;
+    private String transactionType;
+
+    public Transaction(int transactionId, Member m, Book b, String type) {
         this.transactionId = transactionId;
         this.memberRef = m;
         this.bookRef = b;
-        this.transactionType = type; // "BORROW" or "RETURN"
+        this.transactionType = type;
+    }
+
+    // Getters
+    public int getTransactionId() { return transactionId; }
+    public Member getMember() { return memberRef; }
+    public Book getBook() { return bookRef; }
+    public String getTransactionType() { return transactionType; }
+
+    @Override
+    public String toString() {
+        return "Transaction{id=" + transactionId +
+               ", type='" + transactionType +
+               "', member='" + memberRef.getName() +
+               "', book='" + bookRef.getTitle() + "'}";
     }
 }
