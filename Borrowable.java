680464129e46f0b31586972280467a6fd7502867
Onion <mchess563@gmail.com>
2026-04-04 00:36:11 +0700

    push

--- Borrowable.java
+++
@@ -1,5 +0,0 @@
-public interface Borrowable {
-    boolean borrow();   // mark item as borrowed
-    boolean returnItem(); // mark item as returned
-    boolean isBorrowed(); // check borrow status
-}
